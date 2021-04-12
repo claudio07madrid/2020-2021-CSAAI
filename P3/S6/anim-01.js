ba544fdc56c038641abf3548fcc6457a7d1e15f6
claudio07madrid <claudio07madrid@hotmail.com>
2021-04-12 19:41:20 +0200

    Ej-09 y Ej-10 hechos

--- P3/S6/anim-01.js
+++ P3/S6/anim-01.js
@@ -13,13 +13,28 @@ const ctx = canvas.getContext("2d");
 let x = 0;
 let y = 0;
 
+//-- Velocidad horizontal del objeto
+let velx =2;
+
 //-- Funcion principal de animacion
 function update() 
 {
   console.log("test");
   //-- Algoritmo de animacion:
   //-- 1) Actualizar posiciones de los elementos
-  //-- Por hacer
+  //-- (física del movimiento rectilíneo uniforme)
+
+
+  //-- Comprobar colisión con borde derecho
+  //-- Si se alcanza la anchura del canvas, se cambia la velocidad
+  //-- de signo (rebote)
+  if (x >= canvas.width) {
+      velx = -velx;
+  }
+
+  //-- Adtualizar la posición
+  x = x + velx;
+
 
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
