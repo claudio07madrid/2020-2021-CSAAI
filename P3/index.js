645e4ca5f2879c4b605c2461594da8220127802a
claudio07madrid <claudio07madrid@hotmail.com>
2021-05-01 16:31:05 +0200

    consigo pintar los ladrillos

--- P3/index.js
+++ P3/index.js
@@ -10,11 +10,12 @@ canvas.height = 450;
 const ctx = canvas.getContext("2d");
 
 //Variables que influyen en la colisión
-var radiobola = 10;
+var radiobola = 10;//radio de la pelota
 var x = canvas.width/2;
-var y = canvas.height-30;
-var difx = 2;
-var dify = -2;
+var y = canvas.height - 30;
+//velocidades
+var velx = 2;
+var vely = -2;
 
 //Dibujamos la bola
 function dibujobola(){
@@ -49,7 +50,7 @@ function keyUpHandler(e) {
 }
 
 //Definimos la raqueta
-var raquetaHeight = 15;
+var raquetaHeight = 10;
 var raquetaWidth = 75;
 var raquetaX = (canvas.width - raquetaWidth)/2;
 
@@ -61,6 +62,17 @@ function dibujoraqueta(){
     ctx.closePath();
 }
 
+//Variables necesarios para los ladrillos
+const ladrillo = {
+    f: 5, //filas
+    c: 9, //columnas
+    alt: 20, //altura de ladrillo
+    anch: 30, //anchura de ladrillo
+    padding: 10, //espacio alrededor del ladrillo
+    visible: true //estado del ladrillo: activo o no
+}
+
+
 //-- Funcion principal de animacion
 function update(){
   console.log("test");
@@ -69,6 +81,54 @@ function update(){
   dibujoraqueta();
   dibujobola();
 
+
+//Estructura de los ladrillos, inicialmente está vacío
+const ladrillos = [];
+
+//Recorrer todas las filas
+for (let i = 0; i < ladrillo.f; i++){
+    ladrillos[i] = []; //Inicializa la fila. Las filas son Arrays que inicialmente estás vacíos.
+    
+    for (let j = 0; j < ladrillo.c; j++){
+        ladrillos[i][j] = {
+            x: (ladrillo.anch + ladrillo.padding) * j,
+            y:(ladrillo.alt + ladrillo.padding) * i,
+            anch: ladrillo.anch,
+            alt: ladrillo.alt,
+            padding: ladrillo.padding,
+            visible: ladrillo.visible
+        };
+    }
+}
+
+
+for (let i = 0; i < ladrillo.f; i++){
+    for(let j = 0; j < ladrillo.c; j++){
+      //si el ladrillo es visible se pinta
+      if (ladrillos[i][j].visible){
+          ctx.beginPath();
+          ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, ladrillo.anch, ladrillo.alt);
+          ctx.fillStyle = 'red';
+          ctx.fill();
+          ctx.closePath();
+      }
+    }
+}
+
+
+  //Definimos el movimiento de la pelota
+
+  if(x + velx > canvas.width - radiobola || x + velx < radiobola){
+      velx = -velx;
+  }
+  if(y + vely < radiobola) {
+      vely = -vely;
+  }else if(y + vely > canvas.height - radiobola - 10){
+      if(x > raquetaX && x <raquetaX + raquetaWidth){
+          vely = -vely;
+      }
+  }
+
   if(rightPressed && raquetaX < canvas.width - raquetaWidth){
       raquetaX += 7;
   }else if(leftPressed && raquetaX > 0) {
@@ -76,6 +136,8 @@ function update(){
   }
 
   //-- 4) Volver a ejecutar update cuando toque
+  x += velx;
+  y += vely;
   requestAnimationFrame(update);
 }
 
