3f2904271e3f6be3b78fd8b4438bc6bef6574b50
claudio07madrid <claudio07madrid@hotmail.com>
2021-05-01 15:42:25 +0200

    dibujamos bola otra vez para poder colisionar

--- P3/index.js
+++ P3/index.js
@@ -9,13 +9,21 @@ canvas.height = 450;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-//-- Coordenadas del objeto
-let x = 0;
-let y = 10;
+//Variables que influyen en la colisión
+var radiobola = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var difx = 2;
+var dify = -2;
 
-//-- Velocidades del objeto
-let velx = 3;
-let vely = 1;
+//Dibujamos la bola
+function dibujobola(){
+    ctx.beginPath();
+    ctx.arc(x, y, radiobola, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
 
 //Definimos el movimiento de la raqueta
 var rightPressed = false;
@@ -41,7 +49,7 @@ function keyUpHandler(e) {
 }
 
 //Definimos la raqueta
-var raquetaHeight = 10;
+var raquetaHeight = 15;
 var raquetaWidth = 75;
 var raquetaX = (canvas.width - raquetaWidth)/2;
 
@@ -57,41 +65,9 @@ function dibujoraqueta(){
 function update(){
   console.log("test");
 
-  //-- Algoritmo de animacion:
-  //-- 1) Actualizar posicion del  elemento
-  //-- (física del movimiento rectilineo uniforme)
-
-   //-- Condicion de rebote en extremos verticales del canvas
-   if (x < 0 || x >= (canvas.width - 10) ) {
-    velx = -velx;
-  }
-
-  //-- Condición de rebote en extremos horizontales del canvas
-  if (y <= 0 || y > 440) {
-    vely = -vely;
-  }
-
-  //-- Actualizar la posición
-  x = x + velx;
-  y = y + vely;
-
-  //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   dibujoraqueta();
-
-  //-- 3) Dibujar los elementos visibles
-  ctx.beginPath();
-    ctx.arc(x, y, 10, 0,Math.PI*2);
-
-    //-- Dibujar
-    ctx.fillStyle = 'red';
-
-    //-- Rellenar
-    ctx.fill();
-
-    //-- Dibujar el trazo
-    ctx.stroke()
-  ctx.closePath();
+  dibujobola();
 
   if(rightPressed && raquetaX < canvas.width - raquetaWidth){
       raquetaX += 7;
