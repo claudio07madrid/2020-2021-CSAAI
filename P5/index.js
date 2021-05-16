700b2db0cd73ab8a287475e5804649fdd8acf019
claudio07madrid <claudio07madrid@hotmail.com>
2021-05-16 15:03:31 +0200

    configurado para segundo video

--- P5/index.js
+++ P5/index.js
@@ -1,6 +1,7 @@
 //----- Obtener elemento de video y configurarlo
 let directo = document.getElementById("directo");
 const video1 = document.getElementById("video1");
+const video2 = document.getElementById("video2");
 const btn_video1 = document.getElementById("btn_video1");
 const btn_video2 = document.getElementById("btn_video2");
 const btn_test = document.getElementById("btn_test");
@@ -14,25 +15,30 @@ video1.width=200;
 video1.height=100;
 
 //-- Imagen de Test usada
-const TEST_IMAGE_URL = "";
+const TEST_IMAGE_URL = "nosignal.gif";
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
  
   //-- Establecer la fuente de la cámara 1
   video1.src="https://drive.google.com/uc?export=download&id=1GkDAsPsC9d6FDHJP6ADcPYGEdGV5YHli";
+  video2.src="https://drive.google.com/uc?export=download&id=11kjC7Eb9FqrIkCp3GDU4WJtoxfWSvtmM";
 
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
+  video2.currentTime = 0;
+  video2.play();
 
   //-- Y en silencio...
   video1.muted;
+  video2.muted;
 
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
@@ -50,4 +56,12 @@ btn_video1.onclick = () => {
     directo.currentTime = video1.currentTime;
     directo.play();
     directo.poster=null;
+
+//-- Botón de Selección de la cámara 1
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video2.currentTime;
+    directo.play();
+    directo.poster=null;
+};
 };
